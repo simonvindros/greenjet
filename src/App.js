7530049e3e1a4d79c5378a6936f74cd0c6ec483f
simonlindqvist <simon.lindqvist@student.kyh.se>
2021-10-12 11:50:45 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Home from '.components/Home'
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
